ee78aeae33d7aa8a64400cbb2070bc00af666cb5
Taras Pushkar <taraspushkar24@gmail.com>
2025-12-25 21:50:39 +0100

    fixed gitignore, added small delay between notification sending

--- bot/notifier.py
+++ bot/notifier.py
@@ -2,6 +2,7 @@ from db.repositories import *
 import asyncio
 from .bot_logic import *
 from .config import NOTIFIER_INTERVAL_MINUTES as M
+from .config import TELEGRAM_NOTIFICATIONS_INTRVAL_MS as T
 
 shutdown_event = asyncio.Event()
 
@@ -9,13 +10,29 @@ async def notifier_loop(db):
     while not shutdown_event.is_set():
         try:
             searches = await get_active_searches(db)
+
             for search in searches:
                 unsent_items = await get_unsent_items_for_search(db, search["id"])
+
                 for item in unsent_items:
                     tg_id = await get_tg_id_by_user_id(db, search["user_id"])
-                    await send_notification(tg_id, item["title"] + "\n" + item["price"] + "\n" + item["url"] + "\n" + item["brand"], item["image_url"])
+
+                    await send_notification(
+                        tg_id,
+                        item["title"] + "\n" +
+                        item["price"] + "\n" +
+                        item["url"] + "\n" +
+                        item["brand"],
+                        item["image_url"]
+                    )
+
                     await mark_item_as_sent(db, search["id"], item["id"])
+
+                    # 🔴 ЗАДЕРЖКА МЕЖДУ СООБЩЕНИЯМИ
+                    await asyncio.sleep(T / 1000)
+
             await asyncio.wait_for(shutdown_event.wait(), timeout=M * 60)
+
         except asyncio.TimeoutError:
             continue
         except Exception as e:
