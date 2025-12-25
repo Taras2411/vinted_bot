feeee8d7989437632627c1909dea34653d52db85
Taras Pushkar <taraspushkar24@gmail.com>
2025-12-25 17:31:47 +0100

    removed useles prints, changed notification format

--- bot/notifier.py
+++ bot/notifier.py
@@ -13,7 +13,7 @@ async def notifier_loop(db):
                 unsent_items = await get_unsent_items_for_search(db, search["id"])
                 for item in unsent_items:
                     tg_id = await get_tg_id_by_user_id(db, search["user_id"])
-                    await send_notification(tg_id, item["title"] + "\n" + item["url"])
+                    await send_notification(tg_id, item["title"] + "\n" + item["price"] + "\n" + item["url"])
                     await mark_item_as_sent(db, search["id"], item["id"])
             await asyncio.wait_for(shutdown_event.wait(), timeout=M * 60)
         except asyncio.TimeoutError:
